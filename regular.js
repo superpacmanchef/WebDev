340a222abdd607af266eee31f3fd0dbe9a0e0714
Isaac Waldron <Isaacwaldron@hotmail.com>
2020-03-31 01:30:42 +0100

    Merging Delete function

--- regular.js
+++ regular.js
@@ -1,4 +1,7 @@
 var alph = false ; 
+var desc = false ; 
+var early = false ; 
+var late = false ; 
 $(document).ready(function(){
 
     setTableUnsorted();
@@ -17,13 +20,52 @@ $(document).ready(function(){
     
     });
 
+    $('#desc').click(function() {
+        if(desc == false) {
+            desc = true;
+            setTableDescSorted();
+        } else {
+            desc = false; 
+            setTableUnsorted();
+        }
+    })
+
+    $("#early").click(function() {
+        if(early == false) {
+            early = true ;
+            console.log("67");
+            setTableEarlySorted()
+        } else {
+            early = false;
+            setTableUnsorted();
+        }
+    })
+
+
+
     $(document).on("click","#modules tr td button.modify", function() { // any button
         console.log("bumss");
+      });
+
+
+
+      $(document).on("click","#modules tr td button.delete", function() { 
+       var id = $(this).val();
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            data : JSON.stringify(data = {"id" : id}),
+            url : 'http://localhost:3000/del',
+            success : function(data){
+                console.log("Successfuly Delted");
+                setTableUnsorted();
+            }
       });
 
     $("#newAdd").click(function(){
         document.querySelector(".modal-Adding").style.display = "block";
     })
+
 
     $('#addProject').click(function() {
         var projectTitle = $('#projectTitle').val();
@@ -66,6 +108,7 @@ $(document).ready(function(){
     })
  
 })
+})
 
 
 
@@ -93,6 +136,25 @@ function setTableSorted(){
             displayTable(data);
     
         })
+}
+
+function setTableDescSorted() {
+    var sorted = getDescModules();
+    sorted.then((data) => {
+
+        $('#modules').empty();
+        displayTable(data);
+    }) 
+}
+
+
+function setTableEarlySorted() {
+    var sorted = getLatestModules();
+    sorted.then((data) => {
+
+        $('#modules').empty();
+        displayTable(data);
+    }) 
 }
 
 function getUnsortedModules(){
@@ -127,12 +189,43 @@ function getAlphModules(){
 })
 }
 
+function getDescModules(){
+    return new Promise((resolve , reject) => {
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : 'http://localhost:3000/sortD',
+            success : function(data){
+                resolve(data);               
+            },
+            error : function(error){
+                reject(error)
+            }
+        })
+    })
+}
+
+function getLatestModules(){
+    return new Promise((resolve , reject) => {
+        $.ajax({
+            type : "POST",
+            contentType : "application/json",
+            url : 'http://localhost:3000/sortEDueDate',
+            success : function(data){
+                resolve(data);               
+            },
+            error : function(error){
+                reject(error)
+            }
+        })
+    })
+}
+
 function displayTable(json){
     $.get('/templates/table.htm' , function(templates) {
         var template = $(templates).filter('#tables').html();
         $('#modules').append(Mustache.render(template,json));
     })
 }
-
 
 
